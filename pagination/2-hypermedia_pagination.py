9a99724c1b28f219b29c679b4824e1f2db66d569
diu <bongban2003@gmail.com>
2025-04-30 21:20:30 +1000

    2 test

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -38,8 +38,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page,int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+            try:
+                assert isinstance(page,int) and page > 0
+                assert isinstance(page_size, int) and page_size > 0
+            except:
+                 return []
 
             dataset = self.dataset()
             start, end = index_range(page, page_size)
