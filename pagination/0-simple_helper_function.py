e719a485dc857c3c1575b5c0701bdab8ae58c535
diu <bongban2003@gmail.com>
2025-04-30 20:52:40 +1000

    1 test

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,11 @@ that takes two integer arguments page and page_size.
 """
 
 def index_range(page: int = 1, page_size: int = 1) -> (int,int):
+    """
+    Function return a tuple of 2
+    containing a start index and an end
+    index corresponding to the range of indexes
+    """
     start = (page - 1)*page_size
     end = page_size*page
     return (start,end)
---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+This module define a function named index_range
+that takes two integer arguments page and page_size.
+"""
+
+import csv
+import math
+from typing import List
+
+def index_range(page: int = 1, page_size: int = 1) -> (int,int):
+    """
+    Function return a tuple of 2
+    containing a start index and an end
+    index corresponding to the range of indexes
+    """
+    start = (page - 1)*page_size
+    end = page_size*page
+    return (start,end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert isinstance(page,int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
+
+            dataset = self.dataset()
+            start, end = index_range(page, page_size)
+
+            if start > len(dataset):
+                 return []
+            return dataset[start:end]
+    
+    
