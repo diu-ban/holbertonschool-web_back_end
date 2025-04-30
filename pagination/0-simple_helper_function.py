a3ad74748122aafedc4ef56075850075f7e9ece2
diu <bongban2003@gmail.com>
2025-04-30 20:39:58 +1000

    test 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ This module define a function named index_range
 that takes two integer arguments page and page_size.
 """
 
-def index_range(page: int = 1, page_size: int = 1) -> tuple(int,int):
+def index_range(page: int = 1, page_size: int = 1) -> (int,int):
     start = (page - 1)*page_size
     end = page_size*page
     return (start,end)
